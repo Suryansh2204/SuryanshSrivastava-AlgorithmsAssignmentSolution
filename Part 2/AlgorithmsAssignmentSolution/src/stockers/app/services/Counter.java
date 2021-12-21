1a0f4ce96b7d2da3a7219eed9246354599ce0404
Suryansh2204 <suryanshsrivastava22@gmail.com>
2021-12-21 18:33:32 +0530

    First Commit

---
+++ Part 2/AlgorithmsAssignmentSolution/src/stockers/app/services/Counter.java
@@ -0,0 +1,16 @@
+
+package stockers.app.services;
+
+public class Counter 
+{
+	public int count(boolean ar[],boolean key)
+	{
+		int c=0;
+		for(int x=0;x<ar.length;x++)
+		{
+			if(ar[x]==key)
+				c++;
+		}
+		return c;
+	}
+}
